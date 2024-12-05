27ca1e2083b23fdb3a700e7cf22a231e42b31149
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-12-04 23:12:32 -0300

    feat: inserting data using dapper

--- Program.cs
+++ Program.cs
@@ -11,14 +11,48 @@ internal class Program
         // connection.Open();
         // //Code...
         // connection.Close();
+        var category = new Category();
+        category.Id = Guid.NewGuid();
+        category.Title = "Amazon AWS";
+        category.Url = "amazon";
+        category.Description = "Categoria destinada a serviços do AWS";
+        category.Order = 8;
+        category.Summary = "AWS Cloud";
+        category.Featured = false;
+
+        // Created query using sql parameters
+        var insertSql = @"INSERT INTO
+                [Category]
+            VALUES (
+                @Id,
+                @Title, 
+                @Url, 
+                @Summary, 
+                @Order, 
+                @Description, 
+                @Featured)";
 
         using (var connection = new SqlConnection(connectionString))
         {
+            //Instead of doing string interpolation, use Sql Parameters to provider values to the query being executed
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+
+            Console.WriteLine($"{rows} linhas inseridas.");
+
             var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
 
-            foreach (var category in categories)
+            foreach (var item in categories)
             {
-                Console.WriteLine($"{category.Id} - {category.Title}");
+                Console.WriteLine($"{item.Id} - {item.Title}");
             }
         }
     }
