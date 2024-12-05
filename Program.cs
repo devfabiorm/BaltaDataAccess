3ddee18298213466e6e6299402aa24f28a886274
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-12-04 22:19:53 -0300

    feat: created a query using dapper to make data accesss easier

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
         const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=true";
-
         // var connection = new SqlConnection(connectionString);
         // connection.Open();
         // //Code...
@@ -13,23 +14,31 @@ internal class Program
 
         using (var connection = new SqlConnection(connectionString))
         {
-            connection.Open();
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
 
-            using (var command = new SqlCommand())
+            foreach (var category in categories)
             {
-                command.Connection = connection;
-                command.CommandType = System.Data.CommandType.Text;
-                command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-                var reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                }
+                Console.WriteLine($"{category.Id} - {category.Title}");
             }
         }
+    }
 
-        Console.WriteLine("Hello, World!");
+    private static void Module1(SqlConnection connection)
+    {
+        connection.Open();
+
+        using (var command = new SqlCommand())
+        {
+            command.Connection = connection;
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+            }
+        }
     }
 }
